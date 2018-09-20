eed7a25a30754cd0b3d2f683575c1059a3154382
Block Source <blcksrc@gmail.com>
2018-09-21 00:10:07 +0100

    users (app) :  create model Job

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Job(models.Model):
+    job_description = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.job_description
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
