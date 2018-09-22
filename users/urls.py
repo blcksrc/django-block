d214789596d6d69365dd88c97d996975f882819c
Block Source <blcksrc@gmail.com>
2018-09-22 01:09:53 +0100

    users (app) : add index and detail user view pages

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:user_id>/', views.detail, name='detail'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("You're at the users index.")
+    latest_user_list = User.objects.order_by('-date_joined')[:5]
+    context = {'latest_user_list': latest_user_list}
+    return render(request, 'users/index.html', context)
+
+def detail(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    return render(request, 'users/detail.html', {'user': user})
