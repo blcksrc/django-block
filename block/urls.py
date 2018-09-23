26ff170e5e4ecc38e96fa315c836971e3a812326
Block Source <blcksrc@gmail.com>
2018-09-23 23:08:11 +0100

    Add Django authentication views

--- block/urls.py
+++ block/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('users/', include('users.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
