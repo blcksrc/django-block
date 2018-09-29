512d8ef09fb42cb5c918f1bbc30b6a43ee001bb4
Block Source <blcksrc@gmail.com>
2018-09-29 09:30:28 +0100

    Add PostegreSQL settings

--- block/settings.py
+++ block/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'block.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangodb',
+        'USER': 'postgres',
+        'PASSWORD': 'djangodb',
+        'HOST': 'localhost',
+        'POST': '5432',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
